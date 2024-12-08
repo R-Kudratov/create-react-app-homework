0bfdee06950fc11f5e94439d6818a0ebfde28698
Rustam Kudratov <kudratov.rustik@gmail.com>
2024-12-08 20:28:44 +0300

    app.js was rewritten with no use of jsx

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,31 +1,44 @@
 // Начало декларативного стиля
 import logo from "./logo.svg";
 import "./App.css";
+import { createElement } from "react";
 // Конец декларативного стиля
 
 // Начало императивного стиля
 function App() {
-	return (
-		// Начало декларативного стиля
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{new Date().getFullYear()}</p>
-			</header>
-		</div>
-		// Конец декларативного стиля
+	// Начало декларативного стиля
+	return createElement(
+		"div",
+		{ className: "App" },
+		createElement(
+			"header",
+			{ className: "App-header" },
+			createElement("img", {
+				src: logo,
+				className: "App-logo",
+				alt: "logo",
+			}),
+			createElement(
+				"p",
+				null,
+				"Edit ",
+				createElement("code", null, "src/App.js"),
+				" and save to reload."
+			),
+			createElement(
+				"a",
+				{
+					className: "App-link",
+					href: "https://reactjs.org",
+					target: "_blank",
+					rel: "noopener noreferrer",
+				},
+				"Learn React"
+			),
+			createElement("p", null, new Date().getFullYear())
+		)
 	);
+	// Конец декларативного стиля
 }
 // Конец императивного стиля
 
